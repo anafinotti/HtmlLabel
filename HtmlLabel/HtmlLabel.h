34884df0f6730083b0146d3e42dd110de46f339e
Ana Finotti <ana.finotti@zflow.com.br>
2020-12-19 21:55:16 +0100

    First commit

---
+++ HtmlLabel/HtmlLabel.h
@@ -0,0 +1,18 @@
+//
+//  HtmlLabel.h
+//  HtmlLabel
+//
+//  Created by Ana Finotti on 19/12/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HtmlLabel.
+FOUNDATION_EXPORT double HtmlLabelVersionNumber;
+
+//! Project version string for HtmlLabel.
+FOUNDATION_EXPORT const unsigned char HtmlLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HtmlLabel/PublicHeader.h>
+
+
